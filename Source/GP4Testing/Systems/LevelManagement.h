1f8094744a2b1ea1291f13fd45d90af2ef30e4a5
Mohammed Tamim <Alexmerceram@hotmail.com>
2024-10-08 10:57:42 +0200

    working on level management

---
+++ Source/GP4Testing/Systems/LevelManagement.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "GameFramework/Actor.h"
+
+
+#include "LevelManagement.generated.h"
+
+
+
+UCLASS()
+class ALevelManagement : public AActor {
+
+	GENERATED_BODY()
+
+public:
+
+
+};
