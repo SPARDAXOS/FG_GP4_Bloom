7bf9c235e34f1879ece402f744584e038fd6a426
Mohammed Tamim <Alexmerceram@hotmail.com>
2024-10-08 11:04:07 +0200

    Added reload input

--- Source/GP4Testing/Systems/LevelManagement.h
+++ Source/GP4Testing/Systems/LevelManagement.h
@@ -1,9 +1,41 @@
 #pragma once
 
+#include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+
+#include <functional>
 
 
 #include "LevelManagement.generated.h"
+
+
+
+using OnOperationCompletedCallback = std::function<void>(void);
+
+
+USTRUCT(BlueprintType)
+struct FLevelOperationSpec {
+
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere)
+	FName name;
+
+	UPROPERTY(VisibleAnywhere)
+	int32 linkageID;
+
+	UPROPERTY(VisibleAnywhere)
+	int32 UUID;
+
+	OnOperationCompletedCallback callback;
+
+	FLevelOperationSpec() 
+		:	name("None"), linkageID(-1), 
+	{
+
+	}
+
+}
 
 
 
