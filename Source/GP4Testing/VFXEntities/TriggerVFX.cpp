839a7147b568f26ac500dc93681926f6d97af4c1
trinakryshchenko <123742852+trinakryshchenko@users.noreply.github.com>
2024-10-15 14:09:04 +0200

    pushed pots and materials again

---
+++ Source/GP4Testing/VFXEntities/TriggerVFX.cpp
@@ -0,0 +1,56 @@
+#include "TriggerVFX.h"
+#include "NiagaraComponent.h"
+
+#include "GP4Testing/Utility/Debugging.h"
+
+ATriggerVFX::ATriggerVFX() {
+
+	PrimaryActorTick.bCanEverTick = true;
+
+	vfx = CreateDefaultSubobject<UNiagaraComponent>("VFX");
+	vfx->bAutoActivate = false;
+	vfx->OnSystemFinished.AddDynamic(this, &ATriggerVFX::OnVFXFinished);
+	timer.SetTriggerOnce(true);
+	timer.SetOnCompletedCallback(std::bind(&ATriggerVFX::OnTimerFinished, this));
+}
+
+
+
+void ATriggerVFX::Tick(float deltaTime) {
+	Super::Tick(deltaTime);
+
+	if (!status)
+		return;
+
+	timer.Update(deltaTime);
+}
+void ATriggerVFX::SetupOnFinishedCallback(FOnVFXFinishedSignature callback) {
+	if (status)
+		return;
+
+	onFinishedCallback = callback;
+}
+void ATriggerVFX::SetupTimer(FOnVFXFinishedSignature callback, float duration) {
+	if (status)
+		return;
+
+	onTimerFinishedCallback = callback;
+	timer.SetLength(duration);
+}
+void ATriggerVFX::Activate() {
+	if (status)
+		return;
+
+	vfx->ActivateSystem();
+	status = true;
+}
+void ATriggerVFX::OnVFXFinished(UNiagaraComponent* bPSystem) {
+	onFinishedCallback.ExecuteIfBound();
+	status = false;
+	timer.Unlock();
+	Debugging::CustomError("VFX FINISHED triggered!");
+}
+void ATriggerVFX::OnTimerFinished() {
+	onTimerFinishedCallback.ExecuteIfBound();
+	Debugging::CustomError("Timer triggered!");
+}
